6c0652c363c4d02f99c4bd9d0837992b303bd2e1
Andre Lim <64576306+andrepattu@users.noreply.github.com>
2020-07-19 17:53:19 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,26 +5,26 @@ from nltk.stem.wordnet import WordNetLemmatizer
 
 # Function to process text in tweet
 def text_processing(tweet):
-    
-    # Removing punctuation
-    def remove_punctuation(tweet):
-        tweet_blob = TextBlob(tweet)
-        return ' '.join(tweet_blob.words)
 
     def handle_emojis(tweet):
         # Smile -- :), : ), :-), (:, ( :, (-:, :')
-        tweet = re.sub(r'(:\s?\)|:-\)|\(\s?:|\(-:|:\'\))', ' EMO_POS ', tweet)
+        tweet = re.sub(r'(:\s?\)|:-\)|\(\s?:|\(-:|:\'\))', ' POSITIVE_EMOTION ', tweet)
         # Laugh -- :D, : D, :-D, xD, x-D, XD, X-D
-        tweet = re.sub(r'(:\s?D|:-D|x-?D|X-?D)', ' EMO_POS ', tweet)
+        tweet = re.sub(r'(:\s?D|:-D|x-?D|X-?D)', ' POSITIVE_EMOTION ', tweet)
         # Love -- <3, :*
-        tweet = re.sub(r'(<3|:\*)', ' EMO_POS ', tweet)
+        tweet = re.sub(r'(<3|:\*)', ' POSITIVE_EMOTION ', tweet)
         # Wink -- ;-), ;), ;-D, ;D, (;,  (-;
-        tweet = re.sub(r'(;-?\)|;-?D|\(-?;)', ' EMO_POS ', tweet)
+        tweet = re.sub(r'(;-?\)|;-?D|\(-?;)', ' POSITIVE_EMOTION ', tweet)
         # Sad -- :-(, : (, :(, ):, )-:
-        tweet = re.sub(r'(:\s?\(|:-\(|\)\s?:|\)-:)', ' EMO_NEG ', tweet)
+        tweet = re.sub(r'(:\s?\(|:-\(|\)\s?:|\)-:)', ' NEGATIVE_EMOTION ', tweet)
         # Cry -- :,(, :'(, :"(
-        tweet = re.sub(r'(:,\(|:\'\(|:"\()', ' EMO_NEG ', tweet)
+        tweet = re.sub(r'(:,\(|:\'\(|:"\()', ' NEGATIVE_EMOTION ', tweet)
         return tweet
+    
+    # Removing leftover punctuation
+    def remove_punctuation(tweet):
+        tweet_blob = TextBlob(tweet)
+        return ' '.join(tweet_blob.words)
     
     # Removing stopwords and unhelpful symbols
     def remove_stopwords(tweet):
